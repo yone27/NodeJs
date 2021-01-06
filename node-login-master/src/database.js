2bcb655a048ceae05dc13c54d55fb569a51944d7
Sistemas Desarrollo/Yoneiker Gonzalez/Analista Programador <ygonzaleza@GrupoItalcambio.int>
2021-01-06 10:27:24 -0400

    borrando demas proyectos

--- node-login-master/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/notes', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
-    .then(db => console.log("db is connect"))
-    .catch(err => console.error(err))
